fcaf09ceff45125e0871e9f3c57d5ae6a788d3a7
uellenberg <JonahUellenberg@hotmail.com>
2021-05-04 09:35:27 -0700

    Initial commit

---
+++ circle.js
@@ -0,0 +1,211 @@
+const {Num} = require("decimalsystem");
+
+if(!window.bundle) window.bundle = {};
+
+window.bundle.circle = () => {
+    $(() => {
+        window.geo = Desmos.Geometry(document.getElementById("geometry"));
+
+        window.geo.setState(state);
+
+        setInterval(() => {
+            const newState = window.geo.getState();
+
+            //The radius is calculated from two points of the circle, and the diameter and circumference are calculated from it.
+            const radius = Math.hypot(Math.abs(newState.objects["1"].x - newState.objects["2"].x), Math.abs(newState.objects["1"].y - newState.objects["2"].y));
+            const diameter = radius * 2;
+            const circumference = diameter * Math.PI;
+
+            //The circumference and diameter's base PI length is put on their labels.
+            newState.objects["2"].label = new Num(circumference).toBase(Math.PI).toString(3);
+            newState.objects["29"].label = new Num(diameter).toBase(Math.PI).toString(4);
+
+            window.geo.setState(newState);
+        }, 100);
+    });
+}
+
+const state = {
+    "version": "4",
+    "objects": {
+        "0": {
+            "id": "0",
+            "type": "circle",
+            "hidden": false,
+            "parents": {
+                "point1": "1",
+                "point2": "2"
+            },
+            "color": "#388c46"
+        },
+        "1": {
+            "hidden": false,
+            "type": "point",
+            "x": 0.04100606379758487,
+            "y": -0.0779060802801137,
+            "parents": {},
+            "id": "1",
+            "color": "#6042a6"
+        },
+        "2": {
+            "hidden": false,
+            "type": "point",
+            "x": 3.1222531198415155,
+            "y": -0.06374801885928516,
+            "parents": {},
+            "id": "2",
+            "color": "#6042a6",
+            "label": "A",
+            "showLabel": true
+        },
+        "3": {
+            "id": "3",
+            "type": "circle",
+            "hidden": true,
+            "parents": {
+                "point1": "2",
+                "point2": "1"
+            },
+            "color": "#388c46"
+        },
+        "6": {
+            "id": "6",
+            "type": "circle",
+            "hidden": true,
+            "parents": {
+                "point1": "7",
+                "point2": "1"
+            },
+            "color": "#388c46"
+        },
+        "7": {
+            "hidden": true,
+            "type": "intersection",
+            "negRoot": false,
+            "parents": {
+                "host1": "3",
+                "host2": "0"
+            },
+            "id": "7",
+            "color": "#6042a6"
+        },
+        "9": {
+            "id": "9",
+            "type": "circle",
+            "hidden": true,
+            "parents": {
+                "point1": "10",
+                "point2": "1"
+            },
+            "color": "#388c46"
+        },
+        "10": {
+            "hidden": true,
+            "type": "intersection",
+            "negRoot": false,
+            "parents": {
+                "host1": "6",
+                "host2": "0"
+            },
+            "id": "10",
+            "color": "#6042a6"
+        },
+        "12": {
+            "id": "12",
+            "type": "circle",
+            "hidden": true,
+            "parents": {
+                "point1": "13",
+                "point2": "1"
+            },
+            "color": "#388c46"
+        },
+        "13": {
+            "hidden": true,
+            "type": "intersection",
+            "negRoot": false,
+            "parents": {
+                "host1": "9",
+                "host2": "0"
+            },
+            "id": "13",
+            "color": "#6042a6"
+        },
+        "17": {
+            "id": "17",
+            "type": "circle",
+            "hidden": true,
+            "parents": {
+                "point1": "18",
+                "point2": "1"
+            },
+            "color": "#388c46"
+        },
+        "18": {
+            "hidden": true,
+            "type": "intersection",
+            "negRoot": false,
+            "parents": {
+                "host1": "12",
+                "host2": "0"
+            },
+            "id": "18",
+            "color": "#6042a6"
+        },
+        "20": {
+            "id": "20",
+            "type": "circle",
+            "hidden": true,
+            "parents": {
+                "point1": "21",
+                "point2": "1"
+            },
+            "color": "#388c46"
+        },
+        "21": {
+            "hidden": true,
+            "type": "intersection",
+            "negRoot": true,
+            "parents": {
+                "host1": "3",
+                "host2": "17"
+            },
+            "id": "21",
+            "color": "#6042a6"
+        },
+        "29": {
+            "id": "29",
+            "type": "segment",
+            "hidden": false,
+            "parents": {
+                "point1": "18",
+                "point2": "7"
+            },
+            "color": "#2d70b3",
+            "label": "A",
+            "showLabel": true
+        },
+        "32": {
+            "id": "32",
+            "type": "segment",
+            "hidden": false,
+            "parents": {
+                "point1": "1",
+                "point2": "2"
+            },
+            "color": "#2d70b3",
+            "showLabel": true
+        }
+    },
+    "viewport": {
+        "centerX": 0,
+        "centerY": 0,
+        "size": 15
+    },
+    "customTools": {},
+    "graphSettings": {
+        "showGrid": false,
+        "showAxes": false,
+        "showAxisNumbers": false
+    }
+};
---
+++ triangle.js
@@ -0,0 +1,250 @@
+const {Num} = require("decimalsystem");
+
+if(!window.bundle) window.bundle = {};
+
+window.bundle.triangle = triangle => {
+    const cos45 = Math.cos(45 * Math.PI/180);
+    const sin45 = Math.sin(45 * Math.PI/180);
+
+    $(() => {
+        window.geo = Desmos.Geometry(document.getElementById("geometry"));
+
+        window.geo.setState(state);
+
+        setInterval(() => {
+            const newState = window.geo.getState();
+
+            //The first leg of the triangle is based on two points, the distance of is calculated below.
+            //The rest just use trig based on that leg and a 45 degree angle.
+            const l1 = Math.hypot(Math.abs(newState.objects["923"].x - newState.objects["924"].x), Math.abs(newState.objects["923"].y - newState.objects["924"].y)) / 2;
+            const h = l1/cos45;
+            const l2 = sin45*h;
+
+            //The numbers are converted to base SQRT2 and put in their respective labels.
+            newState.objects["948"].label = new Num(l1).toBase(Math.SQRT2).toNumber(0).toLocaleString();
+            newState.objects["949"].label = new Num(l2).toBase(Math.SQRT2).toNumber(0).toLocaleString();
+            newState.objects["940"].label = new Num(h).toBase(Math.SQRT2).toNumber(0).toLocaleString();
+
+            window.geo.setState(newState);
+        }, 100);
+    });
+}
+
+const state = {
+    "version": "4",
+    "objects": {
+        "922": {
+            "id": "922",
+            "type": "segment",
+            "hidden": true,
+            "parents": {
+                "point1": "923",
+                "point2": "924"
+            },
+            "color": "#2d70b3"
+        },
+        "923": {
+            "hidden": false,
+            "type": "point",
+            "x": -4.464597902097905,
+            "y": 0.019667832167829857,
+            "parents": {},
+            "id": "923",
+            "color": "#6042a6",
+            "label": "B",
+            "showLabel": true
+        },
+        "924": {
+            "hidden": true,
+            "type": "point",
+            "x": 1.7373251748251732,
+            "y": -0.05900349650349934,
+            "parents": {},
+            "id": "924",
+            "color": "#6042a6",
+            "label": "A",
+            "showLabel": true
+        },
+        "927": {
+            "id": "927",
+            "type": "circle",
+            "hidden": true,
+            "parents": {
+                "point1": "923",
+                "point2": "929"
+            },
+            "color": "#388c46"
+        },
+        "929": {
+            "hidden": true,
+            "type": "glider",
+            "t": 0.7039013115156058,
+            "parents": {
+                "host": "922"
+            },
+            "id": "929",
+            "color": "#6042a6"
+        },
+        "932": {
+            "id": "932",
+            "type": "segment",
+            "hidden": true,
+            "parents": {
+                "point1": "923",
+                "point2": "929"
+            },
+            "color": "#2d70b3"
+        },
+        "937": {
+            "id": "937",
+            "type": "compass",
+            "hidden": true,
+            "parents": {
+                "straight": "932",
+                "center": "924"
+            },
+            "color": "#388c46"
+        },
+        "940": {
+            "id": "940",
+            "type": "segment",
+            "hidden": false,
+            "parents": {
+                "point1": "923",
+                "point2": "942"
+            },
+            "color": "#2d70b3",
+            "label": "H",
+            "showLabel": true
+        },
+        "942": {
+            "hidden": false,
+            "type": "intersection",
+            "negRoot": false,
+            "parents": {
+                "host1": "937",
+                "host2": "927"
+            },
+            "id": "942",
+            "color": "#6042a6"
+        },
+        "945": {
+            "id": "945",
+            "type": "perpendicular",
+            "hidden": true,
+            "parents": {
+                "straight": "932",
+                "point": "942"
+            },
+            "color": "#2d70b3"
+        },
+        "948": {
+            "type": "segment",
+            "id": "948",
+            "hidden": false,
+            "parents": {
+                "point1": "923",
+                "point2": "953"
+            },
+            "color": "#2d70b3",
+            "label": "L1",
+            "showLabel": true
+        },
+        "949": {
+            "type": "segment",
+            "id": "949",
+            "hidden": false,
+            "parents": {
+                "point1": "953",
+                "point2": "942"
+            },
+            "color": "#2d70b3",
+            "label": "L2",
+            "showLabel": true
+        },
+        "951": {
+            "id": "951",
+            "type": "polygon",
+            "hidden": false,
+            "parents": {
+                "points": [
+                    "923",
+                    "953",
+                    "942"
+                ]
+            },
+            "color": "#2d70b3"
+        },
+        "953": {
+            "hidden": false,
+            "type": "intersection",
+            "negRoot": false,
+            "parents": {
+                "host1": "945",
+                "host2": "932"
+            },
+            "id": "953",
+            "color": "#6042a6"
+        },
+        "962": {
+            "id": "962",
+            "type": "segment",
+            "hidden": false,
+            "parents": {
+                "point1": "953",
+                "point2": "942"
+            },
+            "color": "#2d70b3"
+        },
+        "967": {
+            "id": "967",
+            "type": "angle",
+            "hidden": false,
+            "showLabel": true,
+            "label": "{angle}",
+            "parents": {
+                "point1": "942",
+                "point2": "953",
+                "point3": "923"
+            },
+            "color": "#000000"
+        },
+        "971": {
+            "id": "971",
+            "type": "angle",
+            "hidden": false,
+            "showLabel": true,
+            "label": "{angle}",
+            "parents": {
+                "point1": "942",
+                "point2": "923",
+                "point3": "953"
+            },
+            "color": "#000000"
+        },
+        "975": {
+            "id": "975",
+            "type": "angle",
+            "hidden": false,
+            "showLabel": true,
+            "label": "{angle}",
+            "parents": {
+                "point1": "923",
+                "point2": "942",
+                "point3": "953"
+            },
+            "color": "#000000"
+        }
+    },
+    "viewport": {
+        "centerX": 0,
+        "centerY": 0,
+        "size": 15
+    },
+    "customTools": {},
+    "graphSettings": {
+        "showGrid": false,
+        "showAxes": false,
+        "showAxisNumbers": false
+    }
+};
