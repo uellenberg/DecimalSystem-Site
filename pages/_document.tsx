272471ffa612a16dfba0bc4aa18ff7dab5683f4d
uellenberg <JonahUellenberg@hotmail.com>
2021-05-08 19:10:26 -0700

    Add google search console

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,8 @@ class Doc extends Document {
                 <Head>
                     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossOrigin="anonymous"/>
                     <script src="https://www.desmos.com/api/v1.7/geometry.js?apiKey=1030b80b9dcd49bfbec8bbd40b383837"/>
+
+                    <meta name="google-site-verification" content="85zIz0m7xePZ1SuPmfJ6x-CulwklY-ROXhaXsdOfPmQ" />
                 </Head>
                 <body>
                 <Main />
