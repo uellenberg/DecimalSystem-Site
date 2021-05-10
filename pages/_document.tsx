3b2dd467a5ca76163dbf2d495510452afe8a1336
uellenberg <JonahUellenberg@hotmail.com>
2021-05-10 11:42:34 -0700

    Update browser version of DecimalSystem

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ class Doc extends Document {
                     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossOrigin="anonymous"/>
                     <script src="https://www.desmos.com/api/v1.7/geometry.js?apiKey=1030b80b9dcd49bfbec8bbd40b383837"/>
                     <script src="https://embed.runkit.com"/>
-                    <script src="https://cdn.jsdelivr.net/npm/decimalsystem@1.0.12/dist/decimalsystem.min.js" integrity="sha256-nRyjkyEyUvRF9D0jOSXsPUKj9FJ2DAL16wCsen7MCkU=" crossOrigin="anonymous"/>
+                    <script src="https://cdn.jsdelivr.net/npm/decimalsystem@1.1.0/dist/decimalsystem.min.js" integrity="sha256-tXuzTfFXQgfRvwj1sTcNHgJAgRbjMFXdeISTUGTC5cM=" crossOrigin="anonymous"/>
 
                     <meta name="google-site-verification" content="85zIz0m7xePZ1SuPmfJ6x-CulwklY-ROXhaXsdOfPmQ" />
                 </Head>
